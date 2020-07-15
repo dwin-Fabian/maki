a70830888c982848785f6b7418a39e885dc9cd2d
dwin-Fabian <67163251+dwin-Fabian@users.noreply.github.com>
2020-07-15 14:59:33 -0400

    maki
    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,17 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyApqp8VFsFGqY5mYHcN098g5HGvcPSLiJg",
+  authDomain: "registro-maki.firebaseapp.com",
+  databaseURL: "https://registro-maki.firebaseio.com",
+  projectId: "registro-maki",
+  storageBucket: "registro-maki.appspot.com",
+  messagingSenderId: "416494009125",
+  appId: "1:416494009125:web:44e9fcd1d995234d68b8cc",
+  measurementId: "G-ZGBS5HS2DP"
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
